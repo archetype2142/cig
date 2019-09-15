61c9f3e4b5fa656884fa49350151510b8623b99d
archetype2142 <malhotraritwick2011@gmail.com>
2019-09-15 07:12:07 +0200

    ASD

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,7 @@
 class AuthController < ApplicationController
 	def index
 		if user_signed_in?
-			redirect_to "http://cigarettes-collect.herokuapp.com"
+			redirect_to "http://cigarettes-collect.herokuapp.com?userId=#{current_user.id}"
 		end
 	end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,12 @@ module Cigs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+	  allow do
+	     origins '*'
+	     resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
+	   end
+	end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
