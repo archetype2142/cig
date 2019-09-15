e8b7d5919fea0d9eee6a6d073154735ff1939edc
archetype2142 <malhotraritwick2011@gmail.com>
2019-09-15 11:08:28 +0200

    ASD

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -5,18 +5,18 @@ class BarcodesController < ApplicationController
   end
 
   def create
+  	Hotspot.create!(
+			lat: params['lat'], 
+			long: params['long'],
+			concentration: 1
+		)		
+
   	user = User.find(params['id'])
   	barcode_data = permitted_params
 
   	@barcode = Barby::Code128B.new(barcode_data)	
 		outputter = Barby::PngOutputter.new(@barcode)
-		
-		Hotspot.create!(
-			lat: permitted_params['lat'], 
-			long: permitted_params['long'],
-			concentration: 1
-		)		
-		
+	
 		path = Rails.root.join("app", "assets", "images", "#{user.id}_barcode.png")
 		File.open(path, 'wb'){|f| f.write outputter.to_png }
 		# user.barcode.attach(filename)
